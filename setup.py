4a764d9f0b9969264302b3999d68b33e507fdfed
inuEbisu_ <13098140@qq.com>
2022-02-17 18:00:28 +0800

    Update setup.py & Add setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="inukit",
-    version="0.9",
-    author="inuEbisu",
-    author_email="inuebisu@qq.com",
-    description="Kit by & for inuEbisu",
-    url="https://inuebisu.cn", 
-    packages=find_packages()
+    name = "inukit",
+    packages = ['inukit'],
+    version = "0.9.1",
+    license = 'MIT',
+    description = "Kit by & for inuEbisu",
+    author = "inuEbisu",
+    author_email = "inuebisu@qq.com",
+    url = "https://github.com/inuEbisu/inukit", 
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
 )
